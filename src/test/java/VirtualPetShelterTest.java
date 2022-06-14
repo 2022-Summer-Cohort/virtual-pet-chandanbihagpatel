914a6fa20d5298bbc7784caea28d1f055f54cdfb
chandanbihagpatel <chandanbihagpatel@gmail.com>
2022-06-14 11:18:49 -0400

    added tests

--- src/test/java/VirtualPetShelterTest.java
+++ src/test/java/VirtualPetShelterTest.java
@@ -7,23 +7,29 @@ public class VirtualPetShelterTest {
     @Test
     public void shouldAddPetToShelterAndReturnPet(){
         VirtualPetShelter underTest = new VirtualPetShelter();
-        VirtualPet petTest = new VirtualPet("Chan", 2, 0, 4);
+        VirtualPet petTest = new OrganicDog("Woof", 10, 100, 0, 0, 400, 50);
         underTest.addPet(petTest);
-        assertEquals(petTest,underTest.adoptPet("Chan"));
+        VirtualPet newPetTest = underTest.adoptPet("Woof");
+        assertEquals(petTest.getName(),newPetTest.getName());
     }
 
     @Test
-    public void shouldIncreaseEnergy(){
-        VirtualPet underTest = new VirtualPet("Chan", 2, 0, 4);
-        underTest.sleep();
-        assertEquals(10, underTest.getEnergy());
+    public void shouldIncreaseHealth(){
+        VirtualPet petTest = new OrganicDog("Woof", 10, 100, 0, 0, 400, 50);
+        petTest.play();
+        petTest.eval();
+        ((OrganicDog)petTest).healthCalc();
+        assertEquals( 112 , ((OrganicDog)petTest).getPetHealth());
+    }
+    @Test
+    public void shouldInreaseLoyality(){
+        VirtualPet petTest = new OrganicDog("Woof", 10, 100, 0, 0, 400, 50);
+        ((OrganicDog) petTest).walk();
+        petTest.eval();
+        ((OrganicDog) petTest).healthCalc();
+        assertEquals(53, ((OrganicDog) petTest).getPetLoyalty() );
     }
 
-    @Test
-    public void shouldIncreaseHunger(){
-        VirtualPet underTest = new VirtualPet("Chan", 2, 0, 4);
-        underTest.sleep();
-        assertEquals(5, underTest.getPetHunger());
-    }
+
 
 }
